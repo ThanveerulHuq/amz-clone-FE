13150b12e3ec749222866a5a68b68d1e182f8e5d
thanveer <thanveerul.haq@nielsen.com>
2021-06-16 00:14:12 +0530

    subheader integrated with api

--- src/components/common/SubHeader.jsx
+++ src/components/common/SubHeader.jsx
@@ -1,20 +1,35 @@
 import React, { Component } from "react";
-import "./SubHeader.css"
+import "./SubHeader.css";
 
 class SubHeader extends Component {
   state = {};
+
+  constructor(props) {
+    super(props);
+    this.state = { categories: [] };
+  }
+
+  componentDidMount() {
+    fetch("http://localhost:8080/api/category")
+      .then((res) => res.json())
+      .then((categories) => {
+        this.setState({ categories });
+      });
+  }
+
+  // Note: the empty deps array [] means
+  // this useEffect will run once
+  // similar to componentDidMount()
+
   render() {
-    return <div className="sub-header row m-0 col-12">
-        <div className="col-1">
-            Categories
-        </div>
-        <div className="col-1">
-            Categories
-        </div>
-        <div className="col-1">
-            Categories
-        </div>
-    </div>;
+    const { categories } = this.state;
+    return (
+      <div className="sub-header row m-0 col-12">
+        {categories.map((category) => (
+          <div className="col-1 p-2" key={category.id}>{category.name}</div>
+        ))}
+      </div>
+    );
   }
 }
 
